21526d1fd53e31b3ab1e639106921124fb66b6b6
Cyril Gravelier <cyril@18ruedivona.eu>
2012-01-18 16:40:05 +0100

    Added my age on the CV

--- routes/index.js
+++ routes/index.js
@@ -16,8 +16,22 @@ exports.contact = function(req, res) {
 };
 
 exports.cv = function(req, res) {
+  // Set 1 year in milliseconds
+  var one_year = 1000 * 60 * 60 * 24 * 365;
+
+  var birthday = new Date();
+  birthday.setFullYear(1985);
+  birthday.setMonth(0);
+  birthday.setDate(18);
+
+  var today = new Date();
+
+  // Calculate difference between today and my birthday, and convert to years
+  var age = Math.floor((today.getTime() - birthday.getTime()) / (one_year));
+
   res.render('cv', {
     layout: false
+    , age: age
     , title: 'CV - Cyril Gravelier'
   })
 };
